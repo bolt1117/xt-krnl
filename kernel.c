f76ab9ab8213b7bc0860e626bad203a7a4e14b8e
bolt1117 <59941020+bolt1117@users.noreply.github.com>
2020-02-27 19:38:11 -0600

    Add files via upload

---
+++ kernel.c
@@ -0,0 +1,24 @@
+void text(char *text)
+{
+	char *mem = (char*)0xb8000;
+	while (*text)
+	{
+		*mem++ = *text++;
+		*mem++ = 0x3;
+	}
+}
+void cls()
+{
+	char *mem = (char*)0xb8000;
+	int i = 0;
+	while (i < 80 * 25 * 2)
+	{
+		mem[i++] = 0;
+	}
+	
+}
+void kernel_main()
+{
+	cls();
+	text("XT Kernel v1.0");
+}
